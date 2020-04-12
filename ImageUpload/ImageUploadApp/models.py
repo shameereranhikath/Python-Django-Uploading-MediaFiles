e85f65500195faa86ea684ac7ec22f7b970df6f4
Shameer Eranhikath <shmr_ekth@yahoo.com>
2020-04-12 15:29:16 +0400

    Settings file and urls file are updated,package name pillow is also installed

--- ImageUpload/ImageUploadApp/models.py
+++ ImageUpload/ImageUploadApp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Article(models.Model):
+    title=models.CharField(max_length=200)
+    description=models.CharField(max_length=300)
+    content=models.CharField(max_length=1000)
+    image=models.ImageField()
